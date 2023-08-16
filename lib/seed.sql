28e61f4039d524007e5622d0b51fb2c016c99d24
Taha <taha.othman@outlook.com>
2023-08-16 10:05:48 -0400

    done

--- lib/seed.sql
+++ lib/seed.sql
@@ -8,22 +8,4 @@ INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
 
-select_all_female_bears_return_name_and_age = '''
-    SELECT bears.name, bears.age FROM bears WHERE sex = 'F';
-'''
 
-select_all_bears_names_and_orders_in_alphabetical_order = '''
-    SELECT name FROM bears ORDER BY name;
-'''
-
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = '''
-    SELECT name, age FROM bears WHERE alive = 1 ORDER BY age;
-'''
-
-select_oldest_bear_and_returns_name_and_age = '''
-    SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;
-'''
-
-select_youngest_bear_and_returns_name_and_age = '''
-    SELECT name, age FROM bears ORDER BY age LIMIT 1;
-'''
