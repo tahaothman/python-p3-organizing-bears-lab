9c06aa74757cdc75c61b2e65632d0fb6b439742b
Taha <taha.othman@outlook.com>
2023-08-16 09:52:28 -0400

    done

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+    id int,
+    name text,
+    age int,
+    sex text,
+    color text,
+    temperament text,
+    alive numeric
+)
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ("Winnie", 4, "F", "Yellow", "Friendly", 1),
+  ("Baloo", 9, "M", "Grey", "Easygoing", 1),
+  ("Paddington", 2, "M", "Brown", "Curious", 1),
+  (NULL, 7, "M", "Black", "Aggressive", 0),
+  ("Pooh", 5, "M", "Yellow", "Hungry", 1), 
+  ("Yogi", 6, "M", "Brown", "Sneaky", 1),
+  ("Smokey", 3, "M", "Grey", "Shy", 1),
+  ("Scarface", 12, "M", "Brown", "Mean", 1);
